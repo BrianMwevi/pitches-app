de350285b612993ea4645c770da890a9a847be87
brianmwevi <mwevibrian@gmail.com>
2022-05-12 16:29:53 +0300

    ft:app -- add init file

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from app.config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+
+def create_app(config_environment):
+    app = Flask(__name__)
+    app.config.from_object(config_options[config_environment])
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    # Register blueprint
+    from app.main import auth, landing, comments
+    from app import views, forms
+    app.register_blueprint(auth.auth)
+    app.register_blueprint(landing.landing)
+    app.register_blueprint(comments.comments)
+
+    return app
